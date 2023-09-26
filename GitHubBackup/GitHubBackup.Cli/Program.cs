6e5cb09818a28ac3ac8bed3a66794cc46b6e66f4
Häuptli Manuel (haeupman) <haeupman@students.zhaw.ch>
2023-09-26 10:26:06 +0200

    add serilog

--- GitHubBackup/GitHubBackup.Cli/Program.cs
+++ GitHubBackup/GitHubBackup.Cli/Program.cs
@@ -1,37 +1,75 @@
 ﻿using System.CommandLine;
 using GitHubBackup.Cli;
+using GitHubBackup.Cli.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+var quietOption = new Option<bool>(
+    aliases: new[] { "-q", "--quiet" },
+    getDefaultValue: () => false,
+    description: "Do not print logs to console"
+);
+
+var logFileOption = new Option<FileInfo?>(
+    aliases: new[] { "-l", "--log-file" },
+    getDefaultValue: () => null,
+    description: "The path to the log file"
+);
+
+var verbosityOption = new Option<LogLevel>(
+    aliases: new[] { "-v", "--verbosity" },
+    getDefaultValue: () => LogLevel.Warning,
+    description: "The verbosity of the logs"
+);
+
+var interactiveOption = new Option<bool>(
+    aliases: new[] { "-i", "--interactive" },
+    getDefaultValue: () => false,
+    description: "Select backup customizations interactively"
+);
 
 var destinationOption = new Option<DirectoryInfo>(
     aliases: new[] { "-d", "--destination" },
     getDefaultValue: () => new DirectoryInfo(Directory.GetCurrentDirectory()),
-    description: "The path to put the backup in");
-
-var promptOption = new Option<bool>(
-    aliases: new[] { "--prompt" },
-    getDefaultValue: () => true,
-    description: "Prompt before backup");
+    description: "The path to put the backup in"
+) { IsRequired = true };
 
 var rootCommand = new RootCommand("Github Backup");
-rootCommand.AddOption(destinationOption);
-rootCommand.AddOption(promptOption);
+rootCommand.AddGlobalOption(verbosityOption);
+rootCommand.AddGlobalOption(quietOption);
+rootCommand.AddGlobalOption(logFileOption);
+rootCommand.AddGlobalOption(interactiveOption);
 
-rootCommand.SetHandler((dest, prompt) => Run(() => Backup(dest, prompt)), destinationOption, promptOption);
+rootCommand.AddOption(destinationOption);
+
+rootCommand.SetHandler(
+    (verbosity, quiet, logFile, interactive, dest) =>
+        Run(verbosity, quiet, logFile, () => Backup(dest, interactive)),
+    verbosityOption,
+    quietOption,
+    logFileOption,
+    interactiveOption,
+    destinationOption
+);
 
 return await rootCommand.InvokeAsync(args);
 
-async Task Run(Func<Task> action)
+async Task Run(LogLevel verbosity, bool quiet, FileSystemInfo? logFile, Func<Task> action)
 {
+    Log.Logger = CreateLoggerConfiguration(verbosity, logFile, quiet).CreateLogger();
     var builder = Host.CreateApplicationBuilder(args);
-    builder.Services.AddHostedService(sp => new ActionService(sp.GetRequiredService<IHostApplicationLifetime>(), async () => await action()));
+    builder.Services.AddHostedService(sp =>
+        new ActionService(sp.GetRequiredService<IHostApplicationLifetime>(), async () => await action()));
+    builder.Services.AddSerilog();
     var host = builder.Build();
     await host.RunAsync();
 }
 
-Task Backup(DirectoryInfo directoryInfo, bool prompt)
+Task Backup(DirectoryInfo destination, bool interactive)
 {
-    if (prompt)
+    if (interactive)
     {
         Console.WriteLine("Do you want to backup? (y/n)");
         if (!new List<ConsoleKey> { ConsoleKey.Y }.Contains(Console.ReadKey().Key))
@@ -39,6 +77,30 @@ Task Backup(DirectoryInfo directoryInfo, bool prompt)
             return Task.CompletedTask;
         }
     }
-    
+
     return Task.CompletedTask;
+}
+
+LoggerConfiguration CreateLoggerConfiguration(LogLevel logLevel, FileSystemInfo? fileSystemInfo, bool quiet)
+{
+    var configuration = new LoggerConfiguration()
+        .MinimumLevel.Is(logLevel.MicrosoftToSerilogLevel());
+
+    if (fileSystemInfo is not null)
+    {
+        configuration.WriteTo.File(
+            fileSystemInfo.FullName,
+            rollOnFileSizeLimit: true,
+            fileSizeLimitBytes: 100_000_000,
+            retainedFileCountLimit: 10,
+            rollingInterval: RollingInterval.Infinite
+        );
+    }
+
+    if (!quiet)
+    {
+        configuration.WriteTo.Console();
+    }
+
+    return configuration;
 }
---
+++ GitHubBackup/GitHubBackup.Cli/Utils/LogLevelExtensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Logging;
+using Serilog.Events;
+
+namespace GitHubBackup.Cli.Utils;
+
+internal static class LogLevelExtensions
+{
+    public static LogEventLevel MicrosoftToSerilogLevel(this LogLevel logLevel)
+    {
+        switch (logLevel)
+        {
+            case LogLevel.None:
+            case LogLevel.Critical:
+                return LogEventLevel.Fatal;
+            case LogLevel.Error:
+                return LogEventLevel.Error;
+            case LogLevel.Warning:
+                return LogEventLevel.Warning;
+            case LogLevel.Information:
+                return LogEventLevel.Information;
+            case LogLevel.Debug:
+                return LogEventLevel.Debug;
+            case LogLevel.Trace:
+            default:
+                return LogEventLevel.Verbose;
+        }
+    }
+}
