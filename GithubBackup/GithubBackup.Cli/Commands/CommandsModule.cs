1f69269af3c3450f0d8de03594edb40f4cc689c9
byCrookie <maeni.h01@hotmail.com>
2023-10-02 20:59:17 +0200

    move commands into commands folder

--- GithubBackup/GithubBackup.Cli/Commands/CommandsModule.cs
+++ GithubBackup/GithubBackup.Cli/Commands/CommandsModule.cs
@@ -1,23 +1,24 @@
-using GithubBackup.Cli.Commands;
+using GithubBackup.Cli.Github;
 using GithubBackup.Cli.Options;
-using GithubBackup.Core;
+using GithubBackup.Cli.Services;
+using GithubBackup.Core.DependencyInjection.Factory;
 using Microsoft.Extensions.DependencyInjection;
-using Serilog;
 
-namespace GithubBackup.Cli;
+namespace GithubBackup.Cli.Commands;
 
-internal static class CliModule
+internal static class CommandsModule
 {
     /// <summary>
     /// Registers all dependencies for the CLI. The order of registrations is important.
-    /// All basic dependencies are registered first, then the cli commands.
+    /// All basic dependencies are registered first, then the cli commands and then the services
+    /// who execute commands.
     /// </summary>
     /// <param name="services">Dependencies are registered on this service collection.</param>
     /// <param name="globalArgs">Global application arguments valid for all commands.</param>
     /// <param name="commandArgs">Command arguments only valid for this command.</param>
     /// <typeparam name="TCliCommand">Type of the cli command's implementation.</typeparam>
     /// <typeparam name="TCommandArgs">Type of the cli command's arguments.</typeparam>
-    public static void AddCli<TCliCommand, TCommandArgs>(
+    public static void AddCommands<TCliCommand, TCommandArgs>(
         this IServiceCollection services,
         GlobalArgs globalArgs,
         TCommandArgs commandArgs
@@ -25,9 +26,12 @@ internal static class CliModule
         where TCommandArgs : class
         where TCliCommand : class, ICliCommand
     {
-        services.AddCore();
-        services.AddSerilog();
-        
-        services.AddCommands<TCliCommand, TCommandArgs>(globalArgs, commandArgs);
+        services.AddGithub();
+
+        services.AddTransient<ICliCommand>(s => s
+            .GetRequiredService<IFactory<GlobalArgs, TCommandArgs, TCliCommand>>()
+            .Create(globalArgs, commandArgs));
+
+        services.AddServices();
     }
 }
