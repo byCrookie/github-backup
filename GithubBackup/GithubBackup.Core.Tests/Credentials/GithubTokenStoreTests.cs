ad8fbef797aa9afe911ca74a4a4acc40b6bf057e
byCrookie <maeni.h01@hotmail.com>
2023-10-14 21:07:00 +0200

    add test

---
+++ GithubBackup/GithubBackup.Core.Tests/Credentials/GithubTokenStoreTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using GithubBackup.Core.Github.Credentials;
+
+namespace GithubBackup.Core.Tests.Credentials;
+
+public class GithubTokenStoreTests
+{
+    [Fact]
+    public void Get_TokenNotSet_ThrowException()
+    {
+        var result = GithubTokenStore.Get;
+        result.Should().Throw<InvalidOperationException>();
+    }
+    
+    [Fact]
+    public void Get_TokenSet_ReturnToken()
+    {
+        const string token = "Token";
+        GithubTokenStore.Set(token);
+        var result = GithubTokenStore.Get();
+        result.Should().Be(token);
+    }
+}
