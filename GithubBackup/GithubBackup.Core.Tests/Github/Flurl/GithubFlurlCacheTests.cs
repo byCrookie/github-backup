5ad98bb289e8e337188c9593c5b0a8ab01f6d233
byCrookie <maeni.h01@hotmail.com>
2023-10-14 20:52:07 +0200

    fix cache tests

--- GithubBackup/GithubBackup.Core.Tests/Github/Flurl/GithubFlurlCacheTests.cs
+++ GithubBackup/GithubBackup.Core.Tests/Github/Flurl/GithubFlurlCacheTests.cs
@@ -1,15 +1,16 @@
 ﻿using System.Net;
 using FluentAssertions;
-using Flurl.Http;
 using Flurl.Http.Testing;
 using GithubBackup.Core.Github.Credentials;
 using GithubBackup.Core.Github.Flurl;
+using GithubBackup.Core.Utils;
 
 namespace GithubBackup.Core.Tests.Github.Flurl;
 
 public class GithubFlurlCacheTests : IDisposable
 {
     private const string Token = "token";
+    private const string TestId = "TEST-ID";
 
     public GithubFlurlCacheTests()
     {
@@ -22,34 +23,37 @@ public class GithubFlurlCacheTests : IDisposable
         const string url = "https://api.github.com/test";
 
         var cachedResponse = new TestPageResponse(new List<TestPageItem>());
-        
+        var notModifiedResponse = string.Empty;
+
         using var httpTest = new HttpTest();
 
         httpTest
             .ForCallsTo(url)
             .WithVerb(HttpMethod.Get)
             .WithoutHeader("If-None-Match", "1")
-            .RespondWithJson(cachedResponse, (int)HttpStatusCode.OK, GetHeaders(new KeyValuePair<string, string>("ETag", "1")));
-        
+            .RespondWithJson(cachedResponse, (int)HttpStatusCode.OK, GetHeaders(
+                new KeyValuePair<string, string>("ETag", "1"),
+                new KeyValuePair<string, string>(TestId, nameof(cachedResponse))
+            ));
+
         httpTest
             .ForCallsTo(url)
             .WithVerb(HttpMethod.Get)
             .WithHeader("If-None-Match", "1")
-            .RespondWith(string.Empty, (int)HttpStatusCode.NotModified, GetHeaders());
+            .RespondWith(notModifiedResponse, (int)HttpStatusCode.NotModified, GetHeaders(
+                new KeyValuePair<string, string>(TestId, nameof(notModifiedResponse))
+            ));
 
         var result1 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
-        
-        var result2 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
+            .GetGithubApiAsync(CancellationToken.None);
 
-        result1.Should().BeSameAs(result2);
-        result1.Should().Be(cachedResponse);
-        result2.Should().Be(cachedResponse);
+        var result2 = await "/test"
+            .GetGithubApiAsync(CancellationToken.None);
+
+        result1.Headers.GetRequired(TestId).Should().Be(nameof(cachedResponse));
+        result2.Headers.GetRequired(TestId).Should().Be(nameof(cachedResponse));
     }
-    
+
     [Fact]
     public async Task GetGithubApiAsync_WhenCacheHitAndETagModified_ReturnMakeCall()
     {
@@ -57,32 +61,34 @@ public class GithubFlurlCacheTests : IDisposable
 
         var cachedResponse = new TestPageResponse(new List<TestPageItem>());
         var newResponse = new TestPageResponse(new List<TestPageItem>());
-        
+
         using var httpTest = new HttpTest();
 
         httpTest
             .ForCallsTo(url)
             .WithVerb(HttpMethod.Get)
             .WithoutHeader("If-None-Match", "1")
-            .RespondWithJson(cachedResponse, (int)HttpStatusCode.OK, GetHeaders(new KeyValuePair<string, string>("ETag", "1")));
-        
+            .RespondWithJson(cachedResponse, (int)HttpStatusCode.OK, GetHeaders(
+                new KeyValuePair<string, string>("ETag", "1"),
+                new KeyValuePair<string, string>(TestId, nameof(cachedResponse))
+            ));
+
         httpTest
             .ForCallsTo(url)
             .WithVerb(HttpMethod.Get)
             .WithHeader("If-None-Match", "1")
-            .RespondWithJson(newResponse, (int)HttpStatusCode.OK, GetHeaders());
+            .RespondWithJson(newResponse, (int)HttpStatusCode.OK, GetHeaders(
+                new KeyValuePair<string, string>(TestId, nameof(newResponse))
+            ));
 
         var result1 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
-        
-        var result2 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
+            .GetGithubApiAsync(CancellationToken.None);
 
-        result1.Should().NotBeSameAs(result2);
-        result1.Should().Be(cachedResponse);
-        result2.Should().Be(newResponse);
+        var result2 = await "/test"
+            .GetGithubApiAsync(CancellationToken.None);
+
+        result1.Headers.GetRequired(TestId).Should().Be(nameof(cachedResponse));
+        result2.Headers.GetRequired(TestId).Should().Be(nameof(newResponse));
     }
 
     [Fact]
@@ -90,51 +96,51 @@ public class GithubFlurlCacheTests : IDisposable
     {
         var response1 = new TestPageResponse(new List<TestPageItem>());
         var response2 = new TestPageResponse(new List<TestPageItem>());
-        
+
         using var httpTest = new HttpTest();
 
         httpTest
-            .RespondWithJson(response1, (int)HttpStatusCode.NotModified, GetHeaders(new KeyValuePair<string, string>("ETag", "1")))
-            .RespondWithJson(response2, (int)HttpStatusCode.OK, GetHeaders(new KeyValuePair<string, string>("ETag", "1")));
+            .RespondWithJson(response1, (int)HttpStatusCode.OK, GetHeaders(
+                new KeyValuePair<string, string>("ETag", "1"),
+                new KeyValuePair<string, string>(TestId, nameof(response1))
+            ))
+            .RespondWithJson(response2, (int)HttpStatusCode.OK, GetHeaders(
+                new KeyValuePair<string, string>("ETag", "2"),
+                new KeyValuePair<string, string>(TestId, nameof(response2))
+            ));
 
         var result1 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
-        
-        var result2 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
+            .GetGithubApiAsync(CancellationToken.None);
 
-        result1.Should().NotBeSameAs(result2);
-        result1.Should().Be(response1);
-        result2.Should().Be(response2);
+        var result2 = await "/test"
+            .GetGithubApiAsync(CancellationToken.None);
+
+        result1.Headers.GetRequired(TestId).Should().Be(nameof(response1));
+        result2.Headers.GetRequired(TestId).Should().Be(nameof(response2));
     }
-    
+
     [Fact]
     public async Task GetGithubApiAsync_WhenCacheHitAndNoETag_ReturnMakeCall()
     {
         var response1 = new TestPageResponse(new List<TestPageItem>());
         var response2 = new TestPageResponse(new List<TestPageItem>());
-        
+
         using var httpTest = new HttpTest();
 
         httpTest
-            .RespondWithJson(response1, (int)HttpStatusCode.OK, GetHeaders())
-            .RespondWithJson(response2, (int)HttpStatusCode.OK, GetHeaders());
+            .RespondWithJson(response1, (int)HttpStatusCode.OK, GetHeaders(new KeyValuePair<string, string>(TestId, nameof(response1))))
+            .RespondWithJson(response2, (int)HttpStatusCode.OK, GetHeaders(new KeyValuePair<string, string>(TestId, nameof(response2))));
 
         var result1 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
-        
-        var result2 = await "/test"
-            .GetGithubApiAsync(CancellationToken.None)
-            .ReceiveJson<TestPageResponse>();
+            .GetGithubApiAsync(CancellationToken.None);
 
-        result1.Should().NotBeSameAs(result2);
-        result1.Should().Be(response1);
-        result2.Should().Be(response2);
+        var result2 = await "/test"
+            .GetGithubApiAsync(CancellationToken.None);
+
+        result1.Headers.GetRequired(TestId).Should().Be(nameof(response1));
+        result2.Headers.GetRequired(TestId).Should().Be(nameof(response2));
     }
-    
+
     private static Dictionary<string, string> GetHeaders(params KeyValuePair<string, string>[] headers)
     {
         var allHeaders = new Dictionary<string, string>
