a9c95c52f8a134b3947db900e818951f84a74f14
byCrookie <maeni.h01@hotmail.com>
2023-10-15 19:17:16 +0200

    add tests for cli

---
+++ GithubBackup/GithubBackup.Cli.Tests/ListExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using GithubBackup.Cli.Utils;
+
+namespace GithubBackup.Cli.Tests;
+
+public class ListExtensionsTests
+{
+    [Fact]
+    public void AddAll_AddItems_ItemAreAdded()
+    {
+        ICollection<string> list = new List<string>
+        {
+            "1"
+        };
+
+        list.AddAll(new[] { "2", "3", "4" });
+        
+        list.Should().BeEquivalentTo(new List<string>
+        {
+            "1",
+            "2",
+            "3",
+            "4"
+        });
+    }
+}
--- GithubBackup/GithubBackup.Cli/Utils/ListExtensions.cs
+++ GithubBackup/GithubBackup.Cli/Utils/ListExtensions.cs
@@ -2,14 +2,12 @@
 
 internal static class ListExtensions
 {
-    public static ICollection<T> AddAll<T>(this ICollection<T> source, IEnumerable<T> items)
+    public static void AddAll<T>(this ICollection<T> source, IEnumerable<T> items)
     {
         foreach (var item in items)
         {
             source.Add(item);
         }
-
-        return source;
     }
     
     public static async IAsyncEnumerable<TResult> SelectAsync<TItem, TResult>(this IEnumerable<TItem> source, Func<TItem, Task<TResult>> selector)
