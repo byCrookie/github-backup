18525caf501a9a6ebe7a8c0e8d9bde8cc6192360
byCrookie <maeni.h01@hotmail.com>
2023-10-25 20:52:34 +0200

    add tests

---
+++ GithubBackup/GithubBackup.Cli.Tests/Commands/Github/Migrations/MigrationsArgsTests.cs
@@ -0,0 +1,147 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using FluentAssertions;
+using GithubBackup.Cli.Commands.Github.Migrate;
+using GithubBackup.Cli.Commands.Github.Migrations;
+using GithubBackup.Cli.Tests.Utils;
+using GithubBackup.Cli.Utils;
+
+namespace GithubBackup.Cli.Tests.Commands.Github.Migrations;
+
+[UsesVerify]
+public class MigrationsArgsTests
+{
+    private readonly MigrationsArguments _migrationsArguments = new();
+
+    [Fact]
+    public async Task InvokeAsync_FlagsArePassedWithDaysOld_FlagsGetParsed()
+    {
+        var rootCommand = new RootCommand();
+        rootCommand.AddGlobalOptions(_migrationsArguments.Options());
+        var subCommand = new Command("sub");
+        
+        subCommand.SetHandler(
+            migrationsArgs =>
+            {
+                migrationsArgs.Should().NotBeNull();
+                migrationsArgs.Export.Should().BeTrue();
+                migrationsArgs.DaysOld.Should().Be(7);
+                migrationsArgs.Since.Should().BeNull();
+            },
+            new MigrationsArgsBinder(_migrationsArguments)
+        );
+        
+        rootCommand.AddCommand(subCommand);
+        await TestCommandline.Build(rootCommand)
+            .InvokeAsync("sub --export --days-old 7");
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_FlagsArePassedWithSince_FlagsGetParsed()
+    {
+        var rootCommand = new RootCommand();
+        rootCommand.AddGlobalOptions(_migrationsArguments.Options());
+        var subCommand = new Command("sub");
+        
+        subCommand.SetHandler(
+            migrationsArgs =>
+            {
+                migrationsArgs.Should().NotBeNull();
+                migrationsArgs.Export.Should().BeTrue();
+                migrationsArgs.DaysOld.Should().BeNull();
+                migrationsArgs.Since.Should().Be(new DateTime(2021, 1, 1));
+            },
+            new MigrationsArgsBinder(_migrationsArguments)
+        );
+        
+        rootCommand.AddCommand(subCommand);
+        await TestCommandline.Build(rootCommand)
+            .InvokeAsync("sub --export --since 2021-01-01");
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_ShortFlagsArePassedWithDaysOld_FlagsGetParsed()
+    {
+        var rootCommand = new RootCommand();
+        rootCommand.AddGlobalOptions(_migrationsArguments.Options());
+        var subCommand = new Command("sub");
+        
+        subCommand.SetHandler(
+            migrationsArgs =>
+            {
+                migrationsArgs.Should().NotBeNull();
+                migrationsArgs.Export.Should().BeTrue();
+                migrationsArgs.DaysOld.Should().Be(7);
+                migrationsArgs.Since.Should().BeNull();
+            },
+            new MigrationsArgsBinder(_migrationsArguments)
+        );
+        
+        rootCommand.AddCommand(subCommand);
+        await TestCommandline.Build(rootCommand)
+            .InvokeAsync("sub -e -d 7");
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_ShortFlagsArePassedWithSince_FlagsGetParsed()
+    {
+        var rootCommand = new RootCommand();
+        rootCommand.AddGlobalOptions(_migrationsArguments.Options());
+        var subCommand = new Command("sub");
+        
+        subCommand.SetHandler(
+            migrationsArgs =>
+            {
+                migrationsArgs.Should().NotBeNull();
+                migrationsArgs.Export.Should().BeTrue();
+                migrationsArgs.DaysOld.Should().BeNull();
+                migrationsArgs.Since.Should().Be(new DateTime(2021, 1, 1));
+            },
+            new MigrationsArgsBinder(_migrationsArguments)
+        );
+        
+        rootCommand.AddCommand(subCommand);
+        await TestCommandline.Build(rootCommand)
+            .InvokeAsync("sub -e -s 2021-01-01");
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_OnlyRequiredArePassed_FlagsGetParsedWithDefaults()
+    {
+        var rootCommand = new RootCommand();
+        rootCommand.AddGlobalOptions(_migrationsArguments.Options());
+        var subCommand = new Command("sub");
+        
+        subCommand.SetHandler(
+            migrationsArgs =>
+            {
+                migrationsArgs.Should().NotBeNull();
+                migrationsArgs.Export.Should().BeTrue();
+                migrationsArgs.DaysOld.Should().BeNull();
+                migrationsArgs.Since.Should().BeNull();
+            },
+            new MigrationsArgsBinder(_migrationsArguments)
+        );
+        
+        rootCommand.AddCommand(subCommand);
+        await TestCommandline.Build(rootCommand).InvokeAsync("sub");
+    }
+    
+    [Fact]
+    public async Task InvokeAsync_SinceAndDaysOld_ValidationFails()
+    {
+        var rootCommand = new RootCommand();
+        rootCommand.AddGlobalOptions(_migrationsArguments.Options());
+        var subCommand = new Command("sub");
+        
+        subCommand.SetHandler(
+            _ => { },
+            new MigrationsArgsBinder(_migrationsArguments)
+        );
+        
+        rootCommand.AddCommand(subCommand);
+        var action = () => TestCommandline.Build(rootCommand).InvokeAsync("sub --since 2021-01-01 --days-old 7");
+        
+        await action.Should().ThrowAsync<Exception>();
+    }
+}
