d3c75311397700cd2455e32db46e2ed434c03aac
byCrookie <maeni.h01@hotmail.com>
2023-10-26 19:26:07 +0200

    add tests for download

--- GithubBackup/GithubBackup.Cli.Tests/Commands/Github/Manual/ManualRunnerTests.cs
+++ GithubBackup/GithubBackup.Cli.Tests/Commands/Github/Manual/ManualRunnerTests.cs
@@ -9,13 +9,17 @@ using GithubBackup.Core.Github.Repositories;
 using GithubBackup.Core.Github.Users;
 using GithubBackup.Core.Utils;
 using GithubBackup.TestUtils.Logging;
+using Meziantou.Xunit;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using Spectre.Console;
 using Spectre.Console.Testing;
+using Environment = GithubBackup.Core.Environment.Environment;
 
 namespace GithubBackup.Cli.Tests.Commands.Github.Manual;
 
 [UsesVerify]
+[DisableParallelization]
 public class ManualRunnerTests
 {
     private readonly TestConsole _ansiConsole = new();
@@ -27,7 +31,7 @@ public class ManualRunnerTests
     private readonly ICredentialStore _credentialStore = Substitute.For<ICredentialStore>();
     private readonly IFileSystem _fileSystem = new MockFileSystem();
     private readonly IDateTimeProvider _dateTimeProvider = Substitute.For<IDateTimeProvider>();
-    
+
     [Fact]
     public async Task RunAsync_HasValidTokenAndWantsToContinue_StopWhenNoRepositories()
     {
@@ -38,7 +42,7 @@ public class ManualRunnerTests
         _credentialStore.LoadTokenAsync(CancellationToken.None).Returns("token");
         _userService.WhoAmIAsync(CancellationToken.None).Returns(user);
         _migrationService.GetMigrationsAsync(CancellationToken.None).Returns(new List<Migration>());
-        
+
         _ansiConsole.Input.PushCharacter('y');
         _ansiConsole.Input.PushKey(ConsoleKey.Enter);
         _ansiConsole.Input.PushCharacter('n');
@@ -50,7 +54,7 @@ public class ManualRunnerTests
 
         await Verify(_ansiConsole.Output);
     }
-    
+
     [Fact]
     public async Task RunAsync_HasValidTokenAndDoesNotWantToContinue_LoginAndStopWhenNoRepositories()
     {
@@ -66,7 +70,7 @@ public class ManualRunnerTests
         var accessToken = new AccessToken("token", "bearer", "scope");
         _authenticationService.PollForAccessTokenAsync(deviceUserCodes.DeviceCode, deviceUserCodes.Interval, CancellationToken.None)
             .Returns(accessToken);
-        
+
         _ansiConsole.Input.PushCharacter('n');
         _ansiConsole.Input.PushKey(ConsoleKey.Enter);
         _ansiConsole.Input.PushCharacter('n');
@@ -78,7 +82,7 @@ public class ManualRunnerTests
         await _credentialStore.Received(1).StoreTokenAsync(accessToken.Token, CancellationToken.None);
         await Verify(_ansiConsole.Output);
     }
-    
+
     [Fact]
     public async Task RunAsync_HasNoToken_LoginAndStopWhenNoRepositories()
     {
@@ -94,7 +98,7 @@ public class ManualRunnerTests
         var accessToken = new AccessToken("token", "bearer", "scope");
         _authenticationService.PollForAccessTokenAsync(deviceUserCodes.DeviceCode, deviceUserCodes.Interval, CancellationToken.None)
             .Returns(accessToken);
-        
+
         _ansiConsole.Input.PushCharacter('n');
         _ansiConsole.Input.PushKey(ConsoleKey.Enter);
         _ansiConsole.Input.PushCharacter('n');
@@ -106,7 +110,7 @@ public class ManualRunnerTests
         await _credentialStore.Received(1).StoreTokenAsync(accessToken.Token, CancellationToken.None);
         await Verify(_ansiConsole.Output);
     }
-    
+
     [Fact]
     public async Task RunAsync_HasInvalidToken_LoginAndStopWhenNoRepositories()
     {
@@ -122,7 +126,7 @@ public class ManualRunnerTests
         var accessToken = new AccessToken("token", "bearer", "scope");
         _authenticationService.PollForAccessTokenAsync(deviceUserCodes.DeviceCode, deviceUserCodes.Interval, CancellationToken.None)
             .Returns(accessToken);
-        
+
         _ansiConsole.Input.PushCharacter('n');
         _ansiConsole.Input.PushKey(ConsoleKey.Enter);
         _ansiConsole.Input.PushCharacter('n');
@@ -134,7 +138,153 @@ public class ManualRunnerTests
         await _credentialStore.Received(1).StoreTokenAsync(accessToken.Token, CancellationToken.None);
         await Verify(_ansiConsole.Output);
     }
-    
+
+    [Fact]
+    public async Task RunAsync_NoMigrations_DoNotBackup()
+    {
+        var runner = CreateRunner();
+
+        var user = new User("test", "test");
+
+        _credentialStore.LoadTokenAsync(CancellationToken.None).Returns("token");
+
+        _ansiConsole.Input.PushCharacter('y');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        _userService.WhoAmIAsync(CancellationToken.None).Returns(user);
+
+        _ansiConsole.Input.PushCharacter('n');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        var migrations = new List<Migration>();
+
+        _dateTimeProvider.Now.Returns(new DateTime(2020, 1, 11));
+
+        _migrationService.GetMigrationsAsync(CancellationToken.None)
+            .Returns(migrations);
+
+        await runner.RunAsync(CancellationToken.None);
+
+        _logger.VerifyLogs();
+
+        await Verify(_ansiConsole.Output);
+    }
+
+    [Fact]
+    public async Task RunAsync_NoValidMigrations_DoNotBackup()
+    {
+        var runner = CreateRunner();
+
+        var user = new User("test", "test");
+
+        _credentialStore.LoadTokenAsync(CancellationToken.None).Returns("token");
+
+        _ansiConsole.Input.PushCharacter('y');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        _userService.WhoAmIAsync(CancellationToken.None).Returns(user);
+
+        _ansiConsole.Input.PushCharacter('n');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        var migrations = new List<Migration>
+        {
+            new(1, MigrationState.Exported, new DateTime(2020, 1, 1)),
+            new(2, MigrationState.Exporting, new DateTime(2020, 1, 6)),
+            new(3, MigrationState.Pending, new DateTime(2020, 1, 10)),
+        };
+
+        _dateTimeProvider.Now.Returns(new DateTime(2020, 1, 11));
+
+        _migrationService.GetMigrationsAsync(CancellationToken.None)
+            .Returns(migrations);
+
+        _migrationService.GetMigrationAsync(1, CancellationToken.None)
+            .Returns(migrations[0]);
+
+        _migrationService.GetMigrationAsync(2, CancellationToken.None)
+            .Returns(migrations[1]);
+
+        _migrationService.GetMigrationAsync(3, CancellationToken.None)
+            .Returns(migrations[2]);
+
+        await runner.RunAsync(CancellationToken.None);
+
+        _logger.VerifyLogs();
+
+        await Verify(_ansiConsole.Output);
+    }
+
+    [Fact]
+    public async Task RunAsync_ValidMigrations_BackupSelected()
+    {
+        var runner = CreateRunner();
+
+        var user = new User("test", "test");
+
+        _credentialStore.LoadTokenAsync(CancellationToken.None).Returns("token");
+
+        _ansiConsole.Input.PushCharacter('y');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        _userService.WhoAmIAsync(CancellationToken.None).Returns(user);
+
+        _ansiConsole.Input.PushCharacter('n');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        var migrations = new List<Migration>
+        {
+            new(1, MigrationState.Exported, new DateTime(2020, 1, 1)),
+            new(2, MigrationState.Exported, new DateTime(2020, 1, 6)),
+            new(3, MigrationState.Exported, new DateTime(2020, 1, 10)),
+        };
+
+        _dateTimeProvider.Now.Returns(new DateTime(2020, 1, 11));
+
+        const int id = 3;
+
+        _migrationService.GetMigrationsAsync(CancellationToken.None)
+            .Returns(migrations);
+
+        _migrationService.GetMigrationAsync(1, CancellationToken.None)
+            .Returns(migrations[0]);
+
+        _migrationService.GetMigrationAsync(2, CancellationToken.None)
+            .Returns(migrations[1]);
+
+        _migrationService.GetMigrationAsync(id, CancellationToken.None)
+            .Returns(migrations[2]);
+
+        _ansiConsole.Profile.Capabilities.Interactive = true;
+
+        _ansiConsole.Input.PushKey(ConsoleKey.DownArrow);
+        _ansiConsole.Input.PushKey(ConsoleKey.Spacebar);
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        const string destinationPath = "test1";
+        var pathInvalid = new Environment(_fileSystem).Root(destinationPath);
+        _ansiConsole.Input.PushText(pathInvalid.FullName);
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        const string destinationValidPath = "test2";
+        var pathValid = new Environment(_fileSystem).Root(destinationValidPath);
+        _fileSystem.Directory.CreateDirectory(pathValid.FullName);
+        _ansiConsole.Input.PushText(pathValid.FullName);
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+
+        _migrationService.DownloadMigrationAsync(Arg.Is<DownloadMigrationOptions>(o => o.Id == id), CancellationToken.None)
+            .Returns(call => _fileSystem.Path.Combine(pathValid.FullName, $"test{call.Arg<DownloadMigrationOptions>().Id}.zip"));
+        
+        _ansiConsole.Input.PushCharacter('n');
+        _ansiConsole.Input.PushKey(ConsoleKey.Enter);
+        
+        await runner.RunAsync(CancellationToken.None);
+
+        _logger.VerifyLogs();
+
+        await Verify(_ansiConsole.Output);
+    }
+
     private ManualBackupRunner CreateRunner()
     {
         var manualBackupArgs = new ManualBackupArgs();
