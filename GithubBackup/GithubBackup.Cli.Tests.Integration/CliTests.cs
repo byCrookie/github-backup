e5bfe2436560ebc926e4de485e502d069e182de1
byCrookie <maeni.h01@hotmail.com>
2023-10-29 13:01:59 +0100

    add tests

---
+++ GithubBackup/GithubBackup.Cli.Tests.Integration/CliTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using FluentAssertions;
+
+namespace GithubBackup.Cli.Tests.Integration;
+
+[UsesVerify]
+public class CliTests
+{
+    [ModuleInitializer]
+    internal static void Initialize() => VerifierSettings
+        .AddScrubber(sb => sb.Replace("ReSharperTestRunner", "ghb"));
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData("--help")]
+    [InlineData("--version")]
+    public async Task RunAsync__(string args)
+    {
+        var textWriter = new StringWriter();
+        Console.SetOut(textWriter);
+
+        var exitCode = await Cli.RunAsync(args.Split(" "));
+            
+        exitCode.Should().Be(0);
+        
+        await Verify(textWriter.ToString()).UseParameters(args);
+    }
+}
