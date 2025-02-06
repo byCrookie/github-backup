f245e680644fc281a2ca29ebf2a26025a79b4b20
byCrookie <github@bycrookie.com>
2025-02-06 21:34:11 +0100

    fix upgrade errors

--- GithubBackup.Core.Tests/Flurl/TextJsonSerializerTests.cs
+++ GithubBackup.Core.Tests/Flurl/TextJsonSerializerTests.cs
@@ -1,18 +1,19 @@
-﻿using FluentAssertions;
-using GithubBackup.Core.Flurl;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using FluentAssertions;
 using GithubBackup.Core.Utils;
 
 namespace GithubBackup.Core.Tests.Flurl;
 
 public class TextJsonSerializerTests
 {
-    private readonly TextJsonSerializer _sut = new();
+    private readonly JsonSerializerOptions _options = new() { Converters = { new JsonStringEnumConverter() } };
 
     [Fact]
     public async Task Serialize_WhenSerializing_ThenIsJson()
     {
         var item = new Item("test", ItemEnum.Test2);
-        var json = _sut.Serialize(item);
+        var json = JsonSerializer.Serialize(item,_options );
         await Verify(json);
     }
     
@@ -22,7 +23,7 @@ public class TextJsonSerializerTests
         const string id = "test";
         var itemEnum = ItemEnum.Test2.GetEnumMemberValue();
         var json = $$"""{"id":"{{id}}","itemEnum":"{{itemEnum}}"}""";
-        var item = _sut.Deserialize<Item>(json);
+        var item = JsonSerializer.Deserialize<Item>(json, _options);
         item.Should().NotBeNull();
         item!.Id.Should().Be("test");
         item.ItemEnum.Should().Be(ItemEnum.Test2);
